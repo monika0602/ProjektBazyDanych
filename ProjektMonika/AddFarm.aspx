c8385c97f218cbf90b0dfea1cc9f1ad2617999e1
Monika Dąbrowska <monikad0602@gmail.com>
2020-01-27 09:47:06 +0100

    Implementation of farm adding and removing

--- ProjektMonika/AddFarm.aspx
+++ ProjektMonika/AddFarm.aspx
@@ -1,3 +1,40 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddFarm.aspx.cs" Inherits="ProjektMonika.AddFarm" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div id="Add">
+         <h1 class="text-center">Dodaj aptekę</h1>
+                    <div class="form-group">
+                        <label for="farm_name">Podaj nazwę:</label>
+                        <input type="text" class="form-control" id="farm_name" name="farm_name" placeholder="Nazwa...">
+                    </div>
+                    <div class="form-group">
+                        <label for="street_name">Podaj ulicę:</label>
+                        <input type="text" class="form-control" id="street_name" name="street_name" placeholder="Ulica...">
+                    </div>
+                     <div class="form-group">
+                        <label for="city">Podaj miasto:</label>
+                        <input type="text" class="form-control" id="city" name="city" placeholder="Miasto...">
+                    </div>
+                     <div class="form-group">
+                        <label for="postcode">Podaj kod pocztowy:</label>
+                        <input type="text" class="form-control" id="postcode" name="postcode" placeholder="Kod pocztowy...">
+                    </div>
+          
+                    
+            <asp:Button runat="server" CommandName="Dodaj" Text="Dodaj aptekę" OnClick="Add_Click"/>
+        </div>
+
+    <div id="Delete">
+
+        <h1 class="text-center">Usuń aptekę</h1>
+                    <div class="form-group">
+                        <label for="del">Wybierz aptekę</label>
+                            <div runat="server" class="form-group" id="farms"></div>
+                    </div>
+                <asp:Button runat="server" CommandName="Usun" Text="Usuń aptekę" OnClick="Delete_Click"/>
+
+    </div>
+
+    <div id="GoToList">
+        <asp:Button ID="GetFarm" runat="server" Text="Przejdź do listy" OnClick="GetFarm_Click" />
+    </div>
 </asp:Content>
--- ProjektMonika/AddFarm.aspx.cs
+++ ProjektMonika/AddFarm.aspx.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,7 +17,79 @@ namespace ProjektMonika
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string connection = ConfigurationManager.ConnectionStrings["connectionStringDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlDataReader rdr = null;
+                SqlCommand com2 = new SqlCommand("GetFarm", con);
+                string stringToDel = "<select class='form-control' name='del' id='del'>";
+                com2.CommandType = CommandType.StoredProcedure;
+                rdr = com2.ExecuteReader();
+                while (rdr.Read())
+                {
+                    stringToDel += "<option value='" + rdr["id_apteki"] + "'>" + rdr["nazwa"] + ", " +  rdr["ulica"] + " " + rdr["miasto"] + ", " + rdr["kod_pocztowy"] + "</option>";
+                }
+                stringToDel += "</select>";
+                farms.InnerHtml = stringToDel;
+                con.Close();
+            }
+        }
+
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+
+            string connection = ConfigurationManager.ConnectionStrings["connectionStringDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand com1 = new SqlCommand("DeleteFarm", con);
+                SqlDataReader rdr = null;
+                var id = Request.Form["del"];
+                com1.CommandType = CommandType.StoredProcedure;
+                com1.Parameters.Add(new SqlParameter("@id_apteki", id));
+                com1.CommandType = CommandType.StoredProcedure;
+                rdr = com1.ExecuteReader();
+                rdr.Close();
+                con.Close();
+            }
+            Response.Redirect("AddFarm.aspx");
 
         }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            string connection = ConfigurationManager.ConnectionStrings["connectionStringDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand com1 = new SqlCommand("AddFarm", con);
+                SqlDataReader rdr = null;
+                string farm_name = Request.Form["farm_name"];
+                string street_name = Request.Form["street_name"];
+                string city = Request.Form["city"];
+                string postcode = Request.Form["postcode"];
+
+                com1.CommandType = CommandType.StoredProcedure;
+                com1.Parameters.Add(new SqlParameter("@nazwa", farm_name));
+                com1.Parameters.Add(new SqlParameter("@ulica", street_name));
+                com1.Parameters.Add(new SqlParameter("@miasto", city));
+                com1.Parameters.Add(new SqlParameter("@kod_pocztowy", postcode));
+
+                com1.CommandType = CommandType.StoredProcedure;
+                rdr = com1.ExecuteReader();
+                rdr.Close();
+                con.Close();
+
+            }
+
+            Response.Redirect("AddFarm.aspx");
+        }
+
+        protected void GetFarm_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("GetFarm.aspx");
+        }
+        
     }
 }
